a0cde4fea0bcbbef2f21140cb32b501b7bc9f72c
wuyudi <asukaminato@duck.com>
2022-11-06 13:39:41 +0800

    lab3

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 10
 
 .text
 main:
@@ -22,3 +22,13 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi t0 x0 1 # t0 = 1
+    addi t2 a0 1 # t2 = a0 + 1
+    addi a0 x0 1 # t1 = 1
+while:
+    beq t0 t2 end # if t0 == t2 : return
+    mul a0 a0 t0 # t1 = t1 * t0
+    addi t0 t0 1 # t0 += 1
+    j while 
+end:
+    ret
--- lab03/list_map.s
+++ lab03/list_map.s
@@ -3,20 +3,21 @@
 .text
 main:
     jal ra, create_default_list
-    add s0, a0, x0 # a0 (and now s0) is the head of node list
+    mv s0, a0 # a0 (and now s0) is the head of node list
 
     # Print the list
-    add a0, s0, x0
+    mv a0, s0
     jal ra, print_list
     # Print a newline
     jal ra, print_newline
 
     # === Calling `map(head, &square)` ===
     # Load function arguments
-    add a0, s0, x0 # Loads the address of the first node into a0
+    mv a0, s0 # Loads the address of the first node into a0
 
     # Load the address of the "square" function into a1 (hint: check out "la" on the green sheet)
     ### YOUR CODE HERE ###
+    la a1 square
 
 
     # Issue the call to map
@@ -32,10 +33,11 @@ main:
     # the square does
 
     # Load function arguments
-    add a0, s0, x0 # Loads the address of the first node into a0
+    mv a0, s0 # Loads the address of the first node into a0
 
     # Load the address of the "decrement" function into a1 (should be very similar to before)
     ### YOUR CODE HERE ###
+    la a1 decrement
 
 
     # Issue the call to map
@@ -52,11 +54,17 @@ main:
 map:
     # Prologue: Make space on the stack and back-up registers
     ### YOUR CODE HERE ###
+    addi sp sp -20
+    sw ra 0(sp)
+    sw a0 4(sp)
+    sw a1 8(sp) 
+    sw s0 12(sp)
+    sw s1 16(sp) # save the address of the function to be called
 
     beq a0, x0, done # If we were given a null pointer (address 0), we're done.
 
-    add s0, a0, x0 # Save address of this node in s0
-    add s1, a1, x0 # Save address of function in s1
+    mv s0, a0 # Save address of this node in s0
+    mv s1, a1 # Save address of function in s1
 
     # Remember that each node is 8 bytes long: 4 for the value followed by 4 for the pointer to next.
     # What does this tell you about how you access the value and how you access the pointer to next?
@@ -64,30 +72,43 @@ map:
     # Load the value of the current node into a0
     # THINK: Why a0?
     ### YOUR CODE HERE ###
+    lw a0 0(s0) # a0 = s0->value
 
     # Call the function in question on that value. DO NOT use a label (be prepared to answer why).
     # Hint: Where do we keep track of the function to call? Recall the parameters of "map".
     ### YOUR CODE HERE ###
+    jalr a1 # call the function in a1
 
     # Store the returned value back into the node
     # Where can you assume the returned value is?
     ### YOUR CODE HERE ###
+    sw a0, 0(s0) # s0->value = f(...)
+    
 
     # Load the address of the next node into a0
     # The address of the next node is an attribute of the current node.
     # Think about how structs are organized in memory.
     ### YOUR CODE HERE ###
+    lw a0 4(s0) # a0 = s0->next
 
     # Put the address of the function back into a1 to prepare for the recursion
     # THINK: why a1? What about a0?
     ### YOUR CODE HERE ###
+    mv a1 s1 # a0 is for return value.
 
     # Recurse
     ### YOUR CODE HERE ###
+    jal ra map
 
 done:
     # Epilogue: Restore register values and free space from the stack
     ### YOUR CODE HERE ###
+    lw ra 0(sp)
+    lw a0 4(sp)
+    lw a1 8(sp) 
+    lw s0 12(sp)
+    lw s1 16(sp)
+    addi sp sp 20
 
     jr ra # Return to caller
 
@@ -116,7 +137,7 @@ loop:                   # do...
     jal ra, malloc      #     Allocate memory for the next node
     sw s1, 0(a0)        #     node->value = i
     sw s0, 4(a0)        #     node->next = last
-    add s0, a0, x0      #     last = node
+    mv s0, a0           #     last = node
     addi s1, s1, 1      #     i++
     addi t0, x0, 10
     bne s1, t0, loop    # ... while i!= 10
@@ -151,3 +172,6 @@ malloc:
     addi a0, x0, 9
     ecall
     jr ra
+# Epilogue
+# To avoid some confusion, I directly push all the registers to the stack. And then pop them all at the end.
+# This is not the most efficient way to do this, but it is the easiest to understand. (This sentence is written by copilot)
